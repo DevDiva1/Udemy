7e2f61e770818d3e110306d600517f1d152d7277
Suchi Jain <suchi24@gmail.com>
2017-10-11 21:54:34 -0400

    Non Repeating character

---
+++ UdemyProblems/NonRepeatingCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UdemyProblems
+{
+    class NonRepeatingCharacter
+    {
+        public static void Main(String[] args)
+        {
+            // NOTE: The following input values will be used for testing your solution.
+            var result = NonRepeating("abcab"); // should return 'c'
+            result = NonRepeating("abab"); // should return null
+            result = NonRepeating("aabbbc"); // should return 'c'
+            result = NonRepeating("aabbdbc"); // should return 'd'
+        }
+
+        public static char? NonRepeating(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return null;
+            }
+
+            var inputChars = s.ToCharArray();
+
+            var hm = new Hashtable();
+            foreach (var c in inputChars)
+            {
+                if (hm.ContainsKey(c))
+                {
+                    var currentCount = (int) hm[c];
+                    hm[c] = currentCount + 1;
+                }
+                else
+                {
+                    hm.Add(c, 1);
+                }
+            }
+            foreach (var c in inputChars)
+            {
+                if ((int)hm[c] == 1)
+                    return c;
+            }
+            return null;
+        }
+    }
+}
+    
--- UdemyProblems/RotationArray.cs
+++ UdemyProblems/RotationArray.cs
@@ -8,24 +8,6 @@ namespace UdemyProblems
 {
     public class RotationArray
     {
-        public static void Main(String[] args)
-        {
-            // NOTE: The following input values will be used for testing your solution.
-            //int[] array1 = { 1, 2, 3, 4, 5, 6, 7 };
-            //int[] array2a = { 4, 5, 6, 7, 8, 1, 2, 3 };
-            //var result = IsRotation(array1, array2a);
-            //int[] array2b = { 4, 5, 6, 7, 1, 2, 3 };
-            //result = IsRotation(array1, array2b);
-            //int[] array2c = { 4, 5, 6, 9, 1, 2, 3 };
-            //// isRotation(array1, array2c) should return false.
-            //int[] array2d = { 4, 6, 5, 7, 1, 2, 3 };
-            //// isRotation(array1, array2d) should return false.
-            //int[] array2e = { 4, 5, 6, 7, 0, 2, 3 };
-            //// isRotation(array1, array2e) should return false.
-            //int[] array2f = { 1, 2, 3, 4, 5, 6, 7 };
-            //// isRotation(array1, array2f) should return true.
-        }
-
         // Implement your solution below.
         public static bool IsRotation(int[] array1, int[] array2)
         {
